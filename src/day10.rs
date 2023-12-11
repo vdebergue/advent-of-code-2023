0d6aaddda79214d6a47a4a74f5deddfc59ba8204
Vincent Debergue <vdebergue+git@gmail.com>
2023-12-12 00:44:02 +0100

    add day10

--- src/day10.rs
+++ src/day10.rs
@@ -7,12 +7,58 @@
 // w = 5 h = 5
 // S p=6 x=1 y=1
 
+use std::collections::HashSet;
+
+use num::Integer;
+
 pub fn run(contents: String) {
     let grid = Grid::parse(&contents);
     let start = grid.start();
-    let n = grid.neighbors_position(start);
+    let start_neighbors = grid.neighbors_position(start);
     // dbg!(&grid);
-    dbg!(n);
+    println!("Grid w={} h={}", grid.width, grid.height);
+    let mut boucle: HashSet<usize> = HashSet::new();
+    for n in start_neighbors {
+        let mut previous = start;
+        let mut current = n;
+        boucle.clear();
+        boucle.insert(current);
+        while let Some(next) = grid.follow_pipe(current, previous) {
+            boucle.insert(next);
+            if grid.chars[next] == 'S' {
+                boucle.insert(next);
+                break;
+            }
+            previous = current;
+            current = next;
+        }
+    }
+    println!("Size is {}, length = {}", boucle.len(), boucle.len() / 2);
+
+    // S might be included, depends on the input...
+    let side_chars: HashSet<char> = "LJ|".chars().collect();
+    let sum = grid
+        .chars
+        .iter()
+        .enumerate()
+        .filter(|(pos, &c)| {
+            if !boucle.contains(pos) {
+                let (y, _) = pos.div_rem(&grid.width);
+                let left = (y * grid.width)..(*pos);
+                let sides = left
+                    .filter(|n| {
+                        let char = grid.chars[*n];
+                        side_chars.contains(&char) && boucle.contains(n)
+                    })
+                    .count();
+                sides % 2 == 1
+            } else {
+                false
+            }
+        })
+        .count();
+
+    println!("Sum is {sum}");
 }
 
 #[derive(Debug)]
@@ -38,8 +84,7 @@ impl Grid {
     }
     fn neighbors_position(&self, pos: usize) -> Vec<usize> {
         let mut out = vec![];
-        let x = pos % self.width;
-        let y = pos - (pos / self.width) * self.width;
+        let (y, x) = pos.div_rem(&self.width);
         if x > 0 {
             out.push(pos - 1)
         }
@@ -53,5 +98,67 @@ impl Grid {
             out.push(pos + self.width);
         }
         out
+    }
+    fn follow_pipe(&self, current: usize, previous: usize) -> Option<usize> {
+        let char = self.chars[current];
+        let next = match char {
+            '|' => {
+                if current == previous + self.width {
+                    Some(current + self.width)
+                } else if current == previous - self.width {
+                    Some(current - self.width)
+                } else {
+                    None
+                }
+            }
+            '-' => {
+                if current == previous + 1 {
+                    Some(current + 1)
+                } else if current == previous - 1 {
+                    Some(current - 1)
+                } else {
+                    None
+                }
+            }
+            'L' => {
+                if current == previous + self.width {
+                    Some(current + 1)
+                } else if current == previous - 1 {
+                    Some(current - self.width)
+                } else {
+                    None
+                }
+            }
+            'J' => {
+                if current == previous + self.width {
+                    Some(current - 1)
+                } else if current == previous + 1 {
+                    Some(current - self.width)
+                } else {
+                    None
+                }
+            }
+            '7' => {
+                if current == previous + 1 {
+                    Some(current + self.width)
+                } else if current == previous - self.width {
+                    Some(current - 1)
+                } else {
+                    None
+                }
+            }
+            'F' => {
+                if current == previous - 1 {
+                    Some(current + self.width)
+                } else if current == previous - self.width {
+                    Some(current + 1)
+                } else {
+                    None
+                }
+            }
+            _ => None,
+        };
+        let neighbors = self.neighbors_position(current);
+        next.filter(|n| neighbors.contains(n))
     }
 }
--- src/day11.rs
+++ src/day11.rs
@@ -1,1 +1,85 @@
-pub fn run(contents: String) {}
+use itertools::Itertools;
+
+pub fn run(contents: String) {
+    let universe = Universe::parse(&contents);
+    println!("Universe size {}", universe.galaxies.len());
+    let s: usize = universe
+        .galaxies
+        .iter()
+        .combinations(2)
+        .map(|v| universe.distance(v[0], v[1], 2))
+        .sum();
+    println!("Sum is {s}");
+
+    let s2: usize = universe
+        .galaxies
+        .iter()
+        .combinations(2)
+        .map(|v| universe.distance(v[0], v[1], 1000000))
+        .sum();
+    println!("Sum is {s2}")
+}
+
+#[derive(Debug)]
+struct Pos {
+    x: usize,
+    y: usize,
+}
+
+impl Pos {
+    fn new(x: usize, y: usize) -> Self {
+        Self { x: x, y: y }
+    }
+}
+
+struct Universe {
+    galaxies: Vec<Pos>,
+}
+
+impl Universe {
+    fn parse(str: &str) -> Self {
+        let galaxies: Vec<Pos> = str
+            .lines()
+            .filter(|l| !l.is_empty())
+            .enumerate()
+            .flat_map(|(y, line)| {
+                let g: Vec<Pos> = line
+                    .chars()
+                    .enumerate()
+                    .filter(|(_, c)| c == &'#')
+                    .map(|(x, _)| Pos::new(x, y))
+                    .collect();
+                g
+            })
+            .collect();
+        Self { galaxies: galaxies }
+    }
+
+    fn distance(&self, p1: &Pos, p2: &Pos, factor: usize) -> usize {
+        let (x1, y1) = (p1.x, p1.y);
+        let (x2, y2) = (p2.x, p2.y);
+        let x_range = (x1.min(x2))..(x1.max(x2));
+        let dx: usize = x_range
+            .map(|x| {
+                let has_galaxy = self.galaxies.iter().any(|g| g.x == x);
+                if has_galaxy {
+                    1
+                } else {
+                    factor
+                }
+            })
+            .sum();
+        let y_range = (y1.min(y2))..(y1.max(y2));
+        let dy: usize = y_range
+            .map(|y| {
+                let has_galaxy = self.galaxies.iter().any(|g| g.y == y);
+                if has_galaxy {
+                    1
+                } else {
+                    factor
+                }
+            })
+            .sum();
+        dx + dy
+    }
+}
